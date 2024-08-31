eec1a3473b3da9550e444846f42c3b1997795666
RavenDevNG <grzesiek25@gmail.com>
2024-08-31 23:07:34 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,11 +1,16 @@
 @echo off
-
 set PYTHON=%~dp0/venv/Scripts/python.exe
-set GIT=
+set GIT=git
 set VENV_DIR=./venv
 set COMMANDLINE_ARGS=--auto-launch
 
 echo *** Checking and updating to new version if possible 
-git pull
+%GIT% fetch
+%GIT% reset --hard origin/main
+
+echo.
+echo *** Applying patches...
+%PYTHON% patch_zluda.py
+
 echo.
 .\zluda\zluda.exe -- %PYTHON% main.py %COMMANDLINE_ARGS%
